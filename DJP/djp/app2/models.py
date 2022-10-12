53b64228a03f28543296eaa882479178c04ae2d9
8249596972 <subratmohanty2309@gmail.com>
2022-10-12 23:29:44 +0530

    add data

--- DJP/djp/app2/models.py
+++ DJP/djp/app2/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Data2(models.Model):
+    SrNo = models.IntegerField(default=False)
+    Name = models.CharField(max_length=200)
+    Gmail = models.EmailField(max_length=200)
+    Age = models.IntegerField(default=False)
+    Mark = models.IntegerField(default=False)
--- DJP/djp/app2/views.py
+++ DJP/djp/app2/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from app1.models import Data
 
 # Create your views here.
+
+def data(request):
+    Dt = Data.objects.all()
+    return render(request,'app2/Data.html',{'Data':Dt})
+
+def data2(request):
+
+    return render(request,'app2/Data2.html',)
