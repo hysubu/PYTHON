aaa3a82ef48d2bdb94bc8e2106bd33d689b2fed3
8249596972 <subratmohanty2309@gmail.com>
2022-10-12 22:57:36 +0530

    first commit

--- DJP/djp/app1/admin.py
+++ DJP/djp/app1/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from app1.models import Data
 
 # Register your models here.
+@admin.register(Data)
+class DataAdmin(admin.ModelAdmin):
+    list_display = ['SrNo','Name','Gmail','Age','Add']
--- DJP/djp/app1/models.py
+++ DJP/djp/app1/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Data(models.Model):
+    SrNo = models.IntegerField(default=False)
+    Name = models.CharField(max_length=200)
+    Gmail = models.EmailField(max_length=200)
+    Age = models.IntegerField(max_length=100)
+    Add = models.CharField(default=False)
