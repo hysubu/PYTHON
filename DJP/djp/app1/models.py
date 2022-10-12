b56a965191f8161de79e3818665b714030ac0c36
8249596972 <subratmohanty2309@gmail.com>
2022-10-12 23:06:22 +0530

    add file

--- DJP/djp/app1/models.py
+++ DJP/djp/app1/models.py
@@ -6,5 +6,5 @@ class Data(models.Model):
     SrNo = models.IntegerField(default=False)
     Name = models.CharField(max_length=200)
     Gmail = models.EmailField(max_length=200)
-    Age = models.IntegerField(max_length=100)
-    Add = models.CharField(default=False)
+    Age = models.IntegerField(default=False)
+    Add = models.CharField(max_length=200)
